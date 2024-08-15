514f354a325ec6ce80f1ece383cbb729ccc78f97
Trutrucat <167733097+Trutrucat@users.noreply.github.com>
2024-08-15 12:37:09 -0700

    adjusted user model to include song schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 
+const songSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  decade: {
+    type: String,
+    required: true,
+  }
+})
+
 const playlistSchema = new mongoose.Schema({
   name: {
     type: String,
